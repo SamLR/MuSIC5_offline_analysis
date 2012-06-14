c9a782f78e77cbb81016483c3360cd63c94a3c56
Sam LR <sam.lindenrathen@gmail.com>
2012-06-14 12:08:13 +0900

    added input files as headers

---
+++ MuSIC5_offline_analysis/include/input_file.h
@@ -0,0 +1,30 @@
+//
+//  input_file.h
+//  MuSIC5_offline_analysis
+//
+//  Created by Sam Cook on 14/06/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+// This is an abstract base class for input files
+// that is designed to give access to each entry
+// within some structure(s)
+// 
+
+
+#ifndef MuSIC5_offline_analysis_input_file_h
+#define MuSIC5_offline_analysis_input_file_h
+
+class input_file {
+public:
+    input_file();
+    virtual ~input_file();
+    virtual void open() = 0;
+    virtual void next_entry() = 0;
+    
+private: 
+    // disable assignment and copy constructors
+    input_file const& operator= (input_file const &);
+    input_file(input_file const&);
+};
+
+#endif
---
+++ MuSIC5_offline_analysis/include/input_root.h
@@ -0,0 +1,19 @@
+//
+//  input_root.h
+//  MuSIC5_offline_analysis
+//
+//  Created by Sam Cook on 14/06/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef MuSIC5_offline_analysis_input_root_h
+#define MuSIC5_offline_analysis_input_root_h
+
+class <#class name#> {
+    <#instance variables#>
+    
+public:
+    <#member functions#>
+};
+
+#endif
