a733b2b1d792bcf289d546a0c8269168cb3c59c3
Andrew Edmonds <andy@andy-Equium-L40.(none)>
2012-06-14 15:17:43 +0900

    Start of visitor_test.cpp

---
+++ MuSIC5_offline_analysis/tests/visitor_test/visitor_test.cpp
@@ -0,0 +1,14 @@
+// visitor_test.cpp
+// -- Test the visitor class
+// Created: 14/06/2012 Andrew Edmonds
+
+#include <iostream>
+
+#include "input_file.h"
+#include "aglorthim.h"
+#include "event.h"
+#include "visitor.h"
+
+int main() {
+	
+}
