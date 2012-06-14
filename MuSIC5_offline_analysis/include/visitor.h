c3a855cb56df78778d8704ab97a93f5f597b6e6a
Sam LR <sam.lindenrathen@gmail.com>
2012-06-14 12:20:39 +0900

    added visitor.h

--- MuSIC5_offline_analysis/include/visitor.h
+++ MuSIC5_offline_analysis/include/visitor.h
@@ -5,10 +5,26 @@
 //  Created by Sam Cook on 14/06/2012.
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
+// Loops over an input file and passes the results to 
+// algorithms
+//
 
 #ifndef MuSIC5_offline_analysis_visitor_h
 #define MuSIC5_offline_analysis_visitor_h
 
+class input_file;
+class algorithm;
+
+class visitor {
+public:
+    visitor();
+    virtual ~visitor();
+    void loop() const; 
+    
+private:
+    input_file const * const input_file_m;
+    algorithm const * const  algorithm_m;
+};
 
 
 #endif
