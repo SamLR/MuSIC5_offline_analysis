c262f23ef6a9b1787ce8f3cb274ad33c7de9fa4f
Sam LR <sam.lindenrathen@gmail.com>
2012-06-14 12:25:40 +0900

    Added algorithm base class

---
+++ MuSIC5_offline_analysis/include/algorithm.h
@@ -0,0 +1,22 @@
+//
+//  algorithm.h
+//  MuSIC5_offline_analysis
+//
+//  Created by Sam Cook on 14/06/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef MuSIC5_offline_analysis_algorithm_h
+#define MuSIC5_offline_analysis_algorithm_h
+
+class entry;
+
+class algorithm {
+public:
+    algorithm();
+    virtual ~algorithm();
+    virtual void process(entry const&) = 0;
+};
+
+
+#endif
