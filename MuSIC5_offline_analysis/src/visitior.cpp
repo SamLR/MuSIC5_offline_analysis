457264cb9366f1020975c12ed536bd591f29029a
Andrew Edmonds <andy@andy-Equium-L40.(none)>
2012-06-14 15:07:24 +0900

    Added vistor.cpp

---
+++ MuSIC5_offline_analysis/src/visitior.cpp
@@ -0,0 +1,24 @@
+// visitor.cpp
+// Created: 14/06/2012 Andrew Edmonds
+
+#include <iostream>
+
+#include "visitor.h"
+
+// Constructor
+visitor::visitor(input_file const* in_file, algorithm const* algthm) {
+	input_file_m = in_file;
+	algorithm_m = algthm;
+}
+
+// Destructor
+visitor::~visitor() {
+}
+
+// loop()
+// Goes through the input_file, grabbing each entry and passing it to the algorithm to process
+visitor::loop() {
+	while (*input_file_m->has_next()) {
+		*algorithm_m->process(*input_file_m->get_entry());
+	}
+}
