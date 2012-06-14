6976ea4b27c4d086768fddcc81d6abe473d5c21a
Sam LR <sam.lindenrathen@gmail.com>
2012-06-14 12:37:01 +0900

    added base class 'entry' (also comments on algorithm)

--- MuSIC5_offline_analysis/include/algorithm.h
+++ MuSIC5_offline_analysis/include/algorithm.h
@@ -5,6 +5,8 @@
 //  Created by Sam Cook on 14/06/2012.
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
+// This is the base class for algorithm
+// 
 
 #ifndef MuSIC5_offline_analysis_algorithm_h
 #define MuSIC5_offline_analysis_algorithm_h
@@ -15,7 +17,7 @@ class algorithm {
 public:
     algorithm();
     virtual ~algorithm();
-    virtual void process(entry const&) = 0;
+    virtual void process(entry const&) = 0;   
 };
 
 
--- MuSIC5_offline_analysis/include/entry.h
+++ MuSIC5_offline_analysis/include/entry.h
@@ -5,10 +5,23 @@
 //  Created by Sam Cook on 14/06/2012.
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
+//  An empty abstract base class to represent entries in input files
 
 #ifndef MuSIC5_offline_analysis_entry_h
 #define MuSIC5_offline_analysis_entry_h
 
+class input_file;
 
+class entry {
+public:
+    entry();
+    virtual ~entry() = 0;
+    inline input_file const* get_source()const {return my_source_file_m;};
+    
+private:
+    input_file const* my_source_file_m;
+};
+
+inline entry::~entry() {}
 
 #endif
