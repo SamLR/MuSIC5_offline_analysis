4402a6bb3cc10eddcdc5beb778d3e03e93280cb9
Andrew Edmonds <andy@andy-Equium-L40.(none)>
2012-06-14 16:15:15 +0900

    Skeleton entry class

---
+++ MuSIC5_offline_analysis/tests/visitor_test/line.cpp
@@ -0,0 +1,13 @@
+// line.cpp
+// Represents a line of a text file (inherits from entry)
+// Created: 14/06/2012 Andrew Edmonds
+
+#include <iostream>
+
+#include "line.h"
+
+line::line() {
+}
+
+line::~line() {
+}
