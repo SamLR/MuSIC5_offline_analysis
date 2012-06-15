4d5314990863b0755a438461fc2aad128f196a6c
Sam LR <sam.lindenrathen@gmail.com>
2012-06-15 16:11:51 +0900

    Dummy midus makes uninteresting midus_entries for testing

--- MuSIC5_offline_analysis/tests/midus_entry/dummy_midus.cpp
+++ MuSIC5_offline_analysis/tests/midus_entry/dummy_midus.cpp
@@ -13,7 +13,29 @@
 #include "../../include/midus_entry.h"
 
 void dummy_midus::loop(){
+    int const n_ch = 2;
+    int const n_hits = 2;
     for (int i = 0; i < n_loops_m; ++i) {
-        TDC_branch t(
+        TDC_branch t;
+        t.entry_id_m = i;
+        t.n_ch_m = n_ch;
+        t.n_hits_m = n_hits;
+        
+        QDC_branch q;
+        q.entry_id_m = i;
+        q.n_ch_m = n_ch;
+        
+        for (int ch = 0; ch < n_ch; ++ch) {
+            q.value_m[ch] = i%ch;
+            for (int hit = 0; hit<n_hits; ++hit) {
+                t.value_m[hit][ch] = (9%ch)%hit;
+            }
+        }
+        midus_entry entry(t, q);
+        
+        for (int alg = 0; alg < get_number_algorithms() ; ++alg) {
+            entry.accept(get_algorithm(alg));
+        }
+        
     }
 }
