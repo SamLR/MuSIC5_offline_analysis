281ab787921c11f8c8c7d57275fd6089ad7732a0
Sam LR <sam.lindenrathen@gmail.com>
2012-06-15 14:22:37 +0900

    basic sketch of the form of the trees for MIDUS

---
+++ MuSIC5_offline_analysis/include/midus_tree_structs.h
@@ -0,0 +1,29 @@
+//
+//  midus_tree_structs.h
+//  MuSIC5_offline_analysis
+//
+//  Created by Sam Cook on 15/06/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef MuSIC5_offline_analysis_midus_tree_structs_h
+#define MuSIC5_offline_analysis_midus_tree_structs_h
+
+struct TDC_branch {
+    TDC_branch(int const n_hits, int const n_ch): n_hits_m(n_hits), n_ch_m(n_ch) {;} ;
+    
+    int const n_hits_m;
+    int const n_ch_m;
+    
+    int entry_id_m;
+    int value_m [n_hits_m][n_ch_m];
+};
+
+struct QDC_branch {
+    QDC_branch(int const n_ch) : n_ch_m(n_ch) {;} ;
+    
+    int entry_id_m;
+    int value_m[n_ch_m];
+};
+
+#endif
