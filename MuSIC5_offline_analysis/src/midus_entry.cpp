a6189b2171e3350b5d28329ba4cb9f2b651f9df9
Andrew Edmonds <example@example.com>
2012-06-15 11:28:38 +0900

    Test of the getter functions - passed

--- MuSIC5_offline_analysis/src/midus_entry.cpp
+++ MuSIC5_offline_analysis/src/midus_entry.cpp
@@ -6,7 +6,14 @@
 
 #include "midus_entry.h"
 
-midus_entry::midus_entry() {std::cout << "midus_entry constructor" << std::endl;}
+midus_entry::midus_entry() {
+	std::cout << "midus_entry constructor" << std::endl;
+	for (int i = 0; i < 5; i++) {
+		QDC_m.push_back(2*i);
+		TDC_m.push_back(3.5*i);
+	}
+	event_number_m = 20;
+}
 
 midus_entry::~midus_entry() {std::cout << "midus_entry destructor" << std::endl;}
 
--- MuSIC5_offline_analysis/tests/midus_test/midus_test.cpp
+++ MuSIC5_offline_analysis/tests/midus_test/midus_test.cpp
@@ -2,10 +2,17 @@
 // -- To test the midus_entry class
 // Created: 15/06/2012 Andrew Edmonds
 
+#include <iostream>
+
 #include "../../include/midus_entry.h"
 
 int main() {
 	midus_entry* test = new midus_entry();
+	
+	for (int i = 0; i < 5; i++) {
+		std::cout << test->get_QDC_value(i) << " " << test->get_TDC_value(i) << std::endl;
+	}
+	std::cout << test->get_event_number() << std::endl;
 	delete test;
 	return 0;
 }
