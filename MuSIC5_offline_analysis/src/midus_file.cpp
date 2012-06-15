b3cdcb4fdb9f4b7d4576696f6c87ca1d13f96f46
Andrew Edmonds <example@example.com>
2012-06-15 21:09:33 +0900

    Some fixes so that I can compile - very slapdash so might want to check

--- MuSIC5_offline_analysis/src/midus_file.cpp
+++ MuSIC5_offline_analysis/src/midus_file.cpp
@@ -2,13 +2,15 @@
 // -- Implements the midus_file methods
 // Created: 15/06/2012 Andrew Edmonds
 
+#include <cstdlib>
+
 #include "midus_file.h"
 #include "midus_entry.h"
 #include "midus_tree_structs.h"
 #include "TTree.h"
 
-midus_file::midus_file(std::string const& filename): 
-TFile(filename, "READ"),filename_m(filename) treename_m(treename){
+midus_file::midus_file(std::string const& filename, std::string const& treename): 
+TFile(filename.c_str(), "READ"),filename_m(filename), treename_m(treename){
     init();
 }
 
@@ -19,22 +21,6 @@ midus_file::~midus_file() {
 void midus_file::loop() {
 	// Call default loop method
 	input_file::loop();
-	std::cout << "Entered loop" << std::endl;
-	
-	// Get the TTrees from the ROOT file
-	TTree* trigger = (TTree*) file_m->Get("Trigger;1");
-	//TTree* scaler = (TTree*) file_m->Get("Scaler;1");
-	
-	//std::cout << trigger->GetEntries() << std::endl;
-	trigger->Print();
-	
-	// FIll the QDC_branch and TDC_branch which will be passed to midus_entry to create the entry
-	/*QDC_branch qdc_br;
-	trigger->SetBranchAddress("QDC/nQDC", &qdc_br.n_ch_m);
-	trigger->SetBranchAddress("QDC/QDC", 
-	
-	trigger->GetEvent(21);
-	std::cout << qdc_br.n_ch_m << std::endl;*/
 	
 	// Loop over all the registered algorithms
 	for (int alg = 0; alg < get_number_algorithms() ; ++alg) {
@@ -47,10 +33,10 @@ void midus_file::init() {
     tree_m = (TTree*) this->Get("Trigger");
     if (!tree_m) {
         std::cerr << "There was a problem opening the tree" << std::endl;
-        exit(1);
+        std::exit(1);
     } else {
-        tree_m->SetBranchAddress("Instance", &q_branch_m.n_ch_m);
-        tree_m->SetBranchAddress("QDC", q_branch_m.value_m);
+        //tree_m->SetBranchAddress("Instance", &q_branch_m.n_ch_m);
+        //tree_m->SetBranchAddress("QDC", q_branch_m.value_m);
     }
     
 }
