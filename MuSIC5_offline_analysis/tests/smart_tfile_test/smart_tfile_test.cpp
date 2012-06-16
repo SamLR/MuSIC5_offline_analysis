75c27375cc854d70a9a67d3fa32884d8c8e08272
Sam LR <sam.lindenrathen@gmail.com>
2012-06-17 01:43:04 +0900

    test of smart_tfile (open root file and print it)

--- MuSIC5_offline_analysis/tests/smart_tfile_test/smart_tfile_test.cpp
+++ MuSIC5_offline_analysis/tests/smart_tfile_test/smart_tfile_test.cpp
@@ -7,3 +7,14 @@
 //
 
 #include <iostream>
+#include "../../include/smart_tfile.h"
+#include "TTree.h"
+
+int main() {
+    smart_tfile* file = smart_tfile::getTFile("run00020.root", "READ");
+    
+    TTree* tree = (TTree*) file->Get("Trigger");
+    tree->Print();
+    
+    return 0;
+}
