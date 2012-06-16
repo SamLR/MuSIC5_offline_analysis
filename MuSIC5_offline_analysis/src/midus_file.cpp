c4e2382e49d52283814134646f47e08b67c7efad
Sam LR <sam.lindenrathen@gmail.com>
2012-06-16 13:19:32 +0900

    merge

--- MuSIC5_offline_analysis/src/midus_file.cpp
+++ MuSIC5_offline_analysis/src/midus_file.cpp
@@ -9,14 +9,10 @@
 #include "midus_tree_structs.h"
 #include "TTree.h"
 
-<<<<<<< HEAD
+
 midus_file::midus_file(std::string const& filename)
-: TFile(filename, "READ"), filename_m(filename), treename_m(treename),
+: TFile(filename, "READ"), filename_m(filename), 
 qdc_tree_m(0), tdc_tree_m(0), scaler_tree_m(0), q_branch_m(), t_branch_m(){
-=======
-midus_file::midus_file(std::string const& filename, std::string const& treename): 
-TFile(filename.c_str(), "READ"),filename_m(filename), treename_m(treename){
->>>>>>> a26520122f4a561989713069a00920175d680e1a
     init();
 }
 
@@ -36,33 +32,12 @@ void midus_file::loop() {
 
 void midus_file::init() {
     // initialise the tree
-<<<<<<< HEAD
-    qdc_tree_m = (TTree*) this->Get("QDC");
-    tdc_tree_m = (TTree*) this->Get("TDC");    
-    scaler_tree_m = (TTree*) this->Get("Scaler");
-    
-    if (!(qdc_tree_m && tdc_tree_m && scaler_tree_m) ) {
-        std::cerr << "There was a problem opening one of the trees" << std::endl;
-        exit(1);
-    } else {
-        qdc_tree_m->SetBranchAddress("Instance", q_branch_m.channel);
-        qdc_tree_m->SetBranchAddress("QDC", q_branch_m.qdc);
-        tdc_tree_m->SetBranchAddress("Instance", t_branch_m.channel);
-        tdc_tree_m->SetBranchAddress("n_hits", t_branch_m.n_hits);
-        tdc_tree_m->SetBranchAddress("TDC", t_branch_m.tdc);
-    }
-    
-    n_entries_m =
-}
-=======
-    tree_m = (TTree*) this->Get("Trigger");
+
+    qdc_tree_m = (TTree*) this->Get("Trigger");
     if (!tree_m) {
         std::cerr << "There was a problem opening the tree" << std::endl;
         std::exit(1);
     } else {
-        //tree_m->SetBranchAddress("Instance", &q_branch_m.n_ch_m);
-        //tree_m->SetBranchAddress("QDC", q_branch_m.value_m);
     }
-    
 }
->>>>>>> a26520122f4a561989713069a00920175d680e1a
+
