0ab2f06f36a20c5d85f2c3ca14bfeffaf0d3206f
Sam LR <sam.lindenrathen@gmail.com>
2012-06-17 01:41:49 +0900

    stylistic updates to smart_tfile and removal of public access to TFile::Close and TFile::Write

---
+++ MuSIC5_offline_analysis/tests/smart_tfile_test/smart_tfile_test.cpp
@@ -0,0 +1,9 @@
+//
+//  smart_tfile_test.cpp
+//  MuSIC5_offline_analysis
+//
+//  Created by Sam Cook on 17/06/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
