7efa2704792d501144612183cfe7d8bcb2ca55a3
Andrew Edmonds <example@example.com>
2012-06-16 22:07:13 +0900

    Test compiles...but fails

--- MuSIC5_offline_analysis/src/tfile_export_algorithm.cpp
+++ MuSIC5_offline_analysis/src/tfile_export_algorithm.cpp
@@ -6,11 +6,11 @@
 
 #include "tfile_export_algorithm.h"
 
-tfile_export_algorithm::tfile_export_algorithm(TFile* *const out_file): out_file_m(out_file) {
+tfile_export_algorithm::tfile_export_algorithm(TFile *const out_file): out_file_m(out_file) {
 }
 
 tfile_export_algorithm::~tfile_export_algorithm() {
-	out_file_m->close(); // will write if no more pointer pointing
+	out_file_m->Close(); // will write if no more pointer pointing
 }
 
 void tfile_export_algorithm::process(midus_entry const *) {
