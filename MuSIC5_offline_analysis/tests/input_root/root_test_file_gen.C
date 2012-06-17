3dc80114dec8a52f3d80116880ab5a785b383d8f
Sam LR <sam.lindenrathen@gmail.com>
2012-06-17 12:30:47 +0900

    can now generate basic midus style root files

--- MuSIC5_offline_analysis/tests/input_root/root_test_file_gen.C
+++ MuSIC5_offline_analysis/tests/input_root/root_test_file_gen.C
@@ -15,23 +15,38 @@
     gROOT->Reset(); 
     // the structure to hold the variables for the branch 
     
-    struct branch { 
-        Int_t some_num;
+    struct in_branch { 
+        Int_t n_hits;
+        Int_t dat[500];
     }; 
-    branch branch; 
+    // n_branches, 1 for ADC values, 1 for T0 (TDC0) and then 2 other TDC channels
+    int n_branches = 4;
+    in_branch branches[4]; 
     // create a new ROOT file 
-    TFile *f = new TFile("test.root","RECREATE"); 
+    TFile *f = new TFile("midus_test_in.root","RECREATE"); 
     // create a TTree 
-    TTree *tree = new TTree("T","staff data from ascii file"); 
+    TTree *tree = new TTree("Trigger","mmmm data!"); 
     // create one branch with all information from the stucture 
-    tree->Branch("numbers",&branch.some_num,"number/I"); 
+    tree->Branch("ADC",&(branches[0]),"n_ch/I:data[n_ch]"); 
+    tree->Branch("TDC0",&(branches[1]),"n_hitsT0/I:data[n_hitsT0]"); 
+    tree->Branch("TDC1",&(branches[2]),"n_hitsT1/I:data[n_hitsT1]"); 
+    tree->Branch("TDC2",&(branches[3]),"n_hitsT2/I:data[n_hitsT2]"); 
     
-    for (int i = 0; i < 500; ++i)
-    {
-        branch.some_num = i;
-        tree->Fill(); 
+    int n_events = 10;
+    
+    for(unsigned int event = 0; event < n_events; ++event) {
+         // number of enteries/branch: static for ADC & T0 measuments (2 & 1 
+         // respectively) but mutable for TDC hits
+        int n_enteries [4] = {2, 1, 5%(event+1), 13%(event+1)};
+        for(unsigned int b = 0; b < n_branches; ++b) {
+            branches[b].n_hits=n_enteries[b];
+            for(unsigned int i = 0; i < n_enteries[b]; ++i) {
+                branches[b].dat[i] = i+b;
+            }
+        }
+        tree->Fill();
     }
-                 // check what the tree looks like 
+    
     tree->Print(); 
     f->Write();
 }
