3d89b7e6e64a186e774c3777aaa8af0e989fc6f5
Sam LR <sam.lindenrathen@gmail.com>
2012-06-17 11:10:59 +0900

    fixed obvious bugs

--- MuSIC5_offline_analysis/tests/tfile_export/tfile_export_test.cpp
+++ MuSIC5_offline_analysis/tests/tfile_export/tfile_export_test.cpp
@@ -6,33 +6,24 @@
 #include "../../include/smart_tfile.h"
 
 int main() {
-	smart_tfile* file;
-	file->getTFile("test.root", "RECREATE");
+	smart_tfile* file = smart_tfile::getTFile("test.root", "RECREATE");
 	tfile_converter_algorithm test(file);
 	
 	// Create some mock branches
-	trigger_branch tr1, tr2;
-	for (int i = 0; i < MAX_TDC_HITS; i++) {
-		tr1.n_tdc[i] = i;
-		tr1.tdc0[i] = 22*i;
+	midus_out_branch tr1, tr2;
+	for (int i = 0; i < n_branches_in_trigger_tree; i++) {
+		tr1.n_entries = i;
+		tr1.data[i] = 33*i;
 		
-		tr2.n_tdc[i] = i;
-		tr2.tdc0[i] = 22*i + 10;
-	}
-	for (int i = 0; i < QDC_CH; i++) {
-		tr1.n_qdc[i] = i;
-		tr1.qdc0[i] = 33*i;
-		
-		tr2.n_qdc[i] = i;
-		tr2.qdc0[i] = 33*i + 10;
+		tr2.n_entries = i;
+		tr2.data[i] = 33*i + 10;
 	}
 	
-	midus_entry* mid = new midus_entry(tr1);
-	midus_entry* mid2 = new midus_entry(tr2);
+    midus_out_branch b []= {tr1, tr2};
+    
+	midus_entry* mid = new midus_entry(b);
 	test.process(mid);
-	test.process(mid2);
 	
-	file->Write();
-	file->Close();
+	file->close();
 	return 0;
 }
