6d5d6b064ba41cc6904cf4b548808ceff0662aed
Andrew Edmonds <example@example.com>
2012-06-17 12:29:43 +0900

    Converter so far

--- MuSIC5_offline_analysis/tests/tfile_export/tfile_export_test.cpp
+++ MuSIC5_offline_analysis/tests/tfile_export/tfile_export_test.cpp
@@ -28,10 +28,28 @@ int main() {
 		tr1[3].data[i] = i*10 + 1;
 	}
 	
+	midus_out_branch tr2 [4]; // 0 = ADC, 1 = TDC0, 2 = TDC1, 3 = TDC2
+	tr2[0].n_entries = 2; // ADC (2 channels - U1 U2)
+	tr2[1].n_entries = 1; // TDC0 (T0 - 1 entry (time when event triggered))
+	tr2[2].n_entries = 15; // TDC1 (channel U1)
+	tr2[3].n_entries = 20; // TDC2 (channel U2)
+	for (int i = 0; i < tr2[0].n_entries; i++) {
+		tr2[0].data[i] = i + 1;
+	}
+	tr2[1].data[0] = 1000;
+	for (int i = 0; i < tr2[2].n_entries; i++) {
+		tr2[2].data[i] = i*3 + 1;
+	}
+	for (int i = 0; i < tr2[3].n_entries; i++) {
+		tr2[3].data[i] = i*20 + 1;
+	}
+	
 	midus_entry* event = new midus_entry(tr1); // an event
+	midus_entry* event2 = new midus_entry(tr2); // another event
+	
+	test->process(event);
 	test->process(event);
 	
 	file->close();
-	
 	return 0;
 }
