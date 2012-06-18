36ef12dadd2df873d4184bad50ab31d66b1bd8ec
Sam LR <sam.lindenrathen@gmail.com>
2012-06-19 01:48:38 +0900

    start refactoring enums etc

---
+++ MuSIC5_offline_analysis/src/conversion_branch_structure.cpp
@@ -0,0 +1,9 @@
+//
+//  conversion_branch_structure.cpp
+//  MuSIC5_offline_analysis
+//
+//  Created by Sam Cook on 19/06/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
