05cba9c6a68dd1fbffbf46874cd526a07b25d3ab
Andrew Edmonds <example@example.com>
2012-06-19 04:15:27 +0900

    Added a help message

--- MuSIC5_offline_analysis/mid2root_converter.cpp
+++ MuSIC5_offline_analysis/mid2root_converter.cpp
@@ -15,17 +15,10 @@
 #include "tfile_converter_algorithm.h"
 #include "hist_branch_channel.h"
 
+void HelpMessage();
+
 int main(int argc, 	char * argv[])
 {
-	// Command line args - string filename to process
-	//					 - optional output file name (xxxx_converted.root)
-	//					 - optional flags to control display of ADC (e.g. -ADC and next number is which ADC to plot)
-	// -i input
-	// -o output
-	// -q qdc channel
-	// -a adc channel
-	// -t tdc channelegitbuild proect not producing binary
-	
 	// The command line arguments
 	std::string input_filename;
 	std::string output_filename;
@@ -40,15 +33,17 @@ int main(int argc, 	char * argv[])
 	int n_tdcs_to_draw = 0; // 0 to 15 (t0, U0...U7, D0...D4, Ge0, Ge1)
 	
 	if (argc < 2) {
-		std::cout << "Incorrect number of arguments" << std::endl;
-		std::cout << "Arguments: -i [input_filename] -o [output_filename] -q \"[qdc_channel_numbers]\" -a \"[adc_channel_numbers]\" " 
-				<< "-t \"[tdc_channel_numbers]\" " << std::endl;
+		HelpMessage();
 		exit(1);
 	}
 	
 	int c;
-	while ((c = getopt(argc, argv, "i:o:q:a:t:")) != -1) {
+	while ((c = getopt(argc, argv, "hi:o:q:a:t:")) != -1) {
 		switch(c) {
+		case 'h':
+			HelpMessage();
+			return 0;
+			break;
 		case 'i':
 			input_filename = optarg;
 			break;
@@ -136,7 +131,12 @@ int main(int argc, 	char * argv[])
     		continue;
    		}
    		std::stringstream histname;
-   		histname << "X" << qdc_chs_to_draw[i] << ".ADC";
+   		if (qdc_chs_to_draw[i] <= qdc_ch_U7) {
+   			histname << "U" << qdc_chs_to_draw[i] - 1 << ".ADC";
+   		}
+   		else {
+   			histname << "D" << qdc_chs_to_draw[i] - qdc_ch_U7 - 1 << ".ADC";
+   		}
    		qdc_ch_hist[i] = new hist_branch_channel(out_file, histname.str().c_str(), qdc_chs_to_draw[i], branch_qdc, 100, 0, 3500);
     }	
     
@@ -167,12 +167,20 @@ int main(int argc, 	char * argv[])
     		continue;
     	}
     	else if (tdc_chs_to_draw[i] == tdc_ch_t0) {
-    		tdc_ch_hist[i] = new hist_branch_channel(out_file, "T0.TDC", 0, branch_tdc0, 100, 0, 3000);
+    		tdc_ch_hist[i] = new hist_branch_channel(out_file, "T0.TDC", 0, branch_tdc0, 100, 0, 500000);
     	}
     	else if (tdc_chs_to_draw[i] > tdc_ch_t0) {
     		std::stringstream histname;
-    		histname << "X" << tdc_chs_to_draw[i] << ".TDC";
-    		tdc_ch_hist[i] = new hist_branch_channel(out_file, histname.str().c_str(), 0, branch_tdc0 + tdc_chs_to_draw[i], 100, 0, 3000);
+    		if (tdc_chs_to_draw[i] <= tdc_ch_U7) {
+    			histname << "U" << tdc_chs_to_draw[i] - 1 << ".TDC";
+    		}
+    		else if (tdc_chs_to_draw[i] > tdc_ch_U7 && tdc_chs_to_draw[i] <= tdc_ch_D4) {
+    			histname << "D" << tdc_chs_to_draw[i] - tdc_ch_U7 - 1 << ".TDC";
+    		}
+    		else if (tdc_chs_to_draw[i] > tdc_ch_D4) {
+    			histname << "Ge" << tdc_chs_to_draw[i] - tdc_ch_D4 - 1 << ".TDC";
+    		}
+    		tdc_ch_hist[i] = new hist_branch_channel(out_file, histname.str().c_str(), 0, branch_tdc0 + tdc_chs_to_draw[i], 100, 0, 500000);
     	}
     }
     
@@ -202,5 +210,18 @@ int main(int argc, 	char * argv[])
     out_file->close();
     
     return 0;
+}
+
+void HelpMessage() {
+	std::cout << "The mid2root_converter converts from a MIDAS ROOT input file to another ROOT file with a different tree structure\n";
+	std::cout << "It can also add histograms of specific channels to the root file\n\n";
+	std::cout << "Command line arguments: " << std::endl;
+	std::cout << "\t -h  --  prints this help message\n";
+	std::cout << "\t -i  --  filename of the input MIDAS ROOT file to convert (e.g. -i example.root)\n";
+	std::cout << "\t -o  --  filename of the output ROOT file\n\t\t (if not given then the output file is called example_converted.root in the same directory as the input file)\n\n";
+	std::cout << "The following commands take an integer or a series of integers separated by spaces and enclosed within \" \"\n";
+	std::cout << "\t -q  --  channel numbers of the channels whose ADC values you want to plot\n\t\t (where 1 = U0, 2 = U1, ..., 8 = U7, 9 = D0, ..., 13 = D4)\n\t\t Example: -q \"1 10\" creates histograms of U0.ADC and D1,ADC\n";
+	std::cout << "\t -a  --  channel numbers of the Ge channels whose ADC values you want to plot\n\t\t (where 0 = Ge0, 1 = Ge1)\n\t\t Example -a 0 creates a histogram of Ge0.ADC\n";
+	std::cout << "\t -t  -- channel numbers of the channels whose TDC values you want to plot\n\t\t (where 0 = TDC0, 1 = U0, ..., 8 = U7, 9 = D0, ..., 13 = D4, 14 = Ge0, 15 = Ge1)\n\t\t Example -t \"12 0 4\" creates histograms of D3.TDC, TDC0 and U3.TDC\n";
 }
 
