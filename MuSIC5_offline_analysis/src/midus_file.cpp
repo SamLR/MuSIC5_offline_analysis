7db4c2737ac398a169aac9aa0ecbcb61a1df9eaa
Andrew Edmonds <example@example.com>
2012-06-19 14:43:17 +0900

    Fixed ADC bug

--- MuSIC5_offline_analysis/src/midus_file.cpp
+++ MuSIC5_offline_analysis/src/midus_file.cpp
@@ -110,7 +110,7 @@ void midus_file::extract_values_to(midus_out_branch* out_branches) const {
         // all QDC channels (0-15) are read out but only using 
         // channels 1-13 (which will become indexes 0-12)
         int calc_ch = (get_qdc_ch(ch) - 1);
-        if ( 0 < calc_ch || calc_ch > 12) continue;
+        if (calc_ch < 0 || calc_ch > 12) continue;
         
         int val = calibration_funcs[qdc_i](calc_ch, get_qdc_val(calc_ch));
         // the values require conversion 
