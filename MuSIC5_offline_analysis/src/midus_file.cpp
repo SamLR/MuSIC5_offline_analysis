b4bd2ecbeaeb4a0dec9405cbab14f2a175f00efb
Sam LR <sam.lindenrathen@gmail.com>
2012-06-20 06:23:54 +0900

    moved application of calibration functions into extract_values_to

--- MuSIC5_offline_analysis/src/midus_file.cpp
+++ MuSIC5_offline_analysis/src/midus_file.cpp
@@ -40,23 +40,6 @@ void midus_file::loop(int const n_events) {
         
         extract_values_to(parallel_branches);
         
-        // Calibrate the values
-        for (int ch = qdc_ch_U0 - 1; ch <= qdc_ch_D4 - 1; ch++) { // QDC
-        	parallel_branches[0].data[ch] = calibration_funcs[qdc_i](ch, parallel_branches[0].data[ch], 0);
-        }
-        for (int ch = phadc_ch_Ge0; ch <= phadc_ch_Ge1; ch++) {
-        	if (ch == phadc_ch_Ge0)
-        		parallel_branches[adc0_i].data[0] = calibration_funcs[adc0_i](ch, parallel_branches[adc0_i].data[0], 0);
-        	else if (ch == phadc_ch_Ge1)
-        		parallel_branches[adc1_i].data[0] = calibration_funcs[adc1_i](ch, parallel_branches[adc1_i].data[0], 0);
-        }
-        for (int ch = tdc_ch_t0; ch < tdc_ch_Ge1; ch++) {
-        	int tdc_ch = branch_tdc0 + ch;
-        	for (int hit = 0; hit < parallel_branches[tdc_ch].n_entries; hit++) {
-        		parallel_branches[tdc_ch].data[hit] = calibration_funcs[tdc_i](tdc_ch, parallel_branches[tdc_ch].data[hit], parallel_branches[branch_tdc0].data[0]);
-        	}
-        }
-        
         midus_entry entry(parallel_branches);
         // Loop over all the registered algorithms
         for (int alg = 0; alg < get_number_algorithms() ; ++alg) {
@@ -130,22 +113,25 @@ void midus_file::extract_values_to(midus_structure::midus_out_branch* out_branch
         // channels 1-13 (which will become indexes 0-12)
         int calc_ch = get_qdc_ch(ch);
 		bool good_dat = is_good_qdc_measure(ch);
-        if (ch < midus_structure::eQDC_U1 || ch > midus_structure::eQDC_D5 || !good_dat) continue;
-        out_branches[0].data[calc_ch - 1] = get_qdc_val(ch);
+        if (   ch < midus_structure::eQDC_U1 
+            || ch > midus_structure::eQDC_D5 
+            || !good_dat) continue;
+        int val = calibration_funcs[qdc_i](calc_ch, get_qdc_val(ch), 0);
+        out_branches[0].data[calc_ch - 1] = val;
     }
     
     // ADC channel 0, just needs copying across
     n_entries = branches_m[adc0_i].n_entries;
     out_branches[midus_structure::eMEB_adc0].n_entries = n_entries;
     for (int ch = 0; ch < n_entries; ++ch) {
-        int val = branches_m[adc0_i].data[ch];
+        int val = calibration_funcs[adc0_i](ch, branches_m[adc0_i].data[ch],0);
         out_branches[midus_structure::eMEB_adc0].data[ch] = val;
     }
     // ADC channel 1 is the same as channel 0
     n_entries = branches_m[adc1_i].n_entries;
     out_branches[midus_structure::eMEB_adc1].n_entries = n_entries;
     for (int ch = 0; ch < n_entries; ++ch) {
-        int val = branches_m[adc1_i].data[ch];
+        int val = calibration_funcs[adc1_i](ch, branches_m[adc1_i].data[ch],0);
         out_branches[midus_structure::eMEB_adc1].data[ch] = val;
     }
     
@@ -156,11 +142,13 @@ void midus_file::extract_values_to(midus_structure::midus_out_branch* out_branch
     for (int hit = 0; hit<n_entries; ++hit) {
         if (!is_good_tdc_measure(hit))  continue;
         
+        // split 
+        
         int const tdc_ch = get_tdc_ch(hit);
-        int const val = calibration_funcs[tdc_i](tdc_ch, get_tdc_val(hit));
+        int const val = get_tdc_val(hit);
         int const branch_no = midus_structure::eMEB_tdc0 + tdc_ch;
         int const ch_hit_no = n_hits[tdc_ch];
-        out_branches[branch_no].data[ch_hit_no] = get_tdc_val(hit);
+        out_branches[branch_no].data[ch_hit_no] = val;
         ++n_hits[tdc_ch];
     }
     
