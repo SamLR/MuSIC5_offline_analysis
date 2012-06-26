f047bf658a6044a847ad2ac7dab12e273d8e7ae1
Sam LR <sam.lindenrathen@gmail.com>
2012-06-26 17:26:40 +0100

    added muon_count which should create basic plot of deg thickness against muon count

---
+++ MuSIC5_offline_analysis/scripts/muon_count.C
@@ -0,0 +1,13 @@
+void muon_coun(){
+	// list tfiles & thicknesses
+	// for each file
+	//		open file
+	//		create tree
+	//		choose specific channel?
+	//		fill histogram with TDC data
+	// 		fit histogram data
+	//		plot hist & save
+	//		extract parameters & get integral of exp paramets
+	//		close tree & file
+	// plot thickness Vs integral of fits
+}
