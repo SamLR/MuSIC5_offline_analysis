f1352ec212d3ae02f811827cee9babf9585011db
Sam Cook <sam@samlr.com>
2013-04-30 08:17:23 +0100

    better fits

--- MuSIC5_offline_analysis/scripts/mu_plus_mu_minus_ratio.py
+++ MuSIC5_offline_analysis/scripts/mu_plus_mu_minus_ratio.py
@@ -31,28 +31,26 @@ from root_utilities import make_hist, make_canvas
 # ===================
 # = Global settings =
 # ===================
-# bin_width = 10
+FAST=False
 bin_width = 100
-# bin_width = 30
-# func_fmt = "[0] + [1]*exp(-x/[2]) + [3]*exp(-x/[4]) + [5]*sin([6]*x)"
 func_fmt = "[0] + [1]*exp(-x/[2]) + [3]*exp(-x/[4])"
+target_mat = "Cu"
 #                      Name        initial val                           lower              upper bounds
-fitting_parameters =(("N_{b}",    lambda x: float(x.GetMaximum()/50.0), lambda x: 0.0   , lambda x: x.GetMaximum()**2 ),
-                     ("N_{c}",    lambda x: float(x.GetMaximum()),      lambda x: 0.0   , lambda x: x.GetMaximum()**2 ),
-                     ("#tau_{c}", lambda x: 163.5,                      lambda x: 162.5 , lambda x: 164.2             ),
-                     # ("#tau_{c}", lambda x: 163.5,                      lambda x: 50.0  , lambda x: 1000.0            ),
-                     ("N_{f}",    lambda x: float(x.GetMaximum()/2.0),  lambda x: 0.0   , lambda x: x.GetMaximum()**2 ),
-                     # ("#tau_{f}", lambda x: 2200.0,                     lambda x: 1000.0, lambda x: 20000.0           ))
-                     ("#tau_{f}", lambda x: 2200.0,                     lambda x: 0.0   , lambda x: 20000.0           ))
-                     # ("#theta",   lambda x: 20.0,                       lambda x: 0.0   , lambda x: 20000.0           ),
-                     # ("N_{sin}",  lambda x: float(x.GetMaximum()/100.0),lambda x: 0.0   , lambda x: 20000.0           ))
-
+fitting_parameters =(("N_{b}",            lambda x: float(x.GetMaximum()/50.0), lambda x: 0.0   , lambda x: x.GetMaximum()**2 ),
+                     ("N_"+target_mat,    lambda x: float(x.GetMaximum()),      lambda x: 0.0   , lambda x: x.GetMaximum()**2 ),
+                     ("#tau_"+target_mat, lambda x: 163.5,                      lambda x: 162.5 , lambda x: 164.2             ),
+                     ("N_{f}",            lambda x: float(x.GetMaximum()/2.0),  lambda x: 0.0   , lambda x: x.GetMaximum()**2 ),
+                     ("#tau_{f}",         lambda x: 2200.0,                     lambda x: 0.0   , lambda x: 20000.0           ))
+# file_fmt = 
+file_fmt = "../../../converted_data/run00{}_converted.root" if target_mat=="Cu" else "../../../converted_Mg_data/run00{}_converted.root"
 # ================
 # = Misc gubbins =
 # ================
 # Useful little function to get nice looking print out
-def pretty_val_and_er(param, val_places="7.1", er_places="7.1"):
-  return u"{%s[0]: >%se} \xb1 {%s[1]: <%se} "%(param, val_places, param, er_places)
+def pretty_val_and_er(param, val_places="10.1", er_places="10.2"):
+  # return u"{%s[0]: >%se} \xb1 {%s[1]: <%se} "%(param, val_places, param, er_places)
+  return u"{%s[0]: >%sf} \xb1 {%s[1]: <%sf} "%(param, val_places, param, er_places)
+  
 # Decorator class for logging entry/exit of functions
 class EntryLogger(object):
   """Logs entries to a function"""
@@ -100,7 +98,6 @@ def process_data_file(file_id, channels, save_name=""):
 # =================
 @EntryLogger
 def get_tree_in_file(file_id):
-  file_fmt = "../../../converted_data/run00{}_converted.root"
   file_name = file_fmt.format(file_id)
   
   file = TFile(file_name, "READ")
@@ -119,9 +116,9 @@ def get_ch_dt_hists(tree, channels):
   assign_leaves(tree, channels)
   hists = make_ch_hists(tree, channels)
   for event_id, event in enumerate(tree):
-    # if (event_id>200000): # FAST
-    #   print "EARLY BREAK!"
-    #   break
+    if (event_id>200000 and FAST): # FAST
+      print "EARLY BREAK!"
+      break
     if (event_id%1000000 == 0): 
       print event_id
     else:
@@ -171,16 +168,16 @@ def fit_dbl_exp(hists, file_id):
   canvas = make_canvas(str(file_id), n_x=3, n_y=2, resize=True)
   for pad_id, (ch, hist) in enumerate(hists.items(), 1):
     pad = canvas.cd(pad_id)
-    func = get_func(hist, ch)
+    func_name = "func_file:{}_ch:_{}".format(hist.file_id, ch)
+    func = get_func(hist, func_name)
     fit_res = hist.Fit(func,"MERS")#"MERNS")
     hist.param = get_parameters(func)
-    hist.covar_matrix = fit_res.GetCovarianceMatrix()
+    # hist.covar_matrix = fit_res.GetCovarianceMatrix()
     pad.Update()
   canvas.Update()
   return canvas
 
-def get_func(hist, ch):
-  name = "func_file:{}_ch:_{}".format(hist.file_id, ch)
+def get_func(hist, name):
 
   func = TF1(name, func_fmt, 50, 20000)
                        
@@ -210,40 +207,46 @@ def get_parameters(func):
 # =======================
 @EntryLogger
 def get_all_integrals(hists):
-  total_ints = {"c":[0.0, 0.0], "f":[0.0, 0.0], "R":[0.0, 0.0]}
+  total_ints = {"f":[0.0, 0.0], target_mat:[0.0, 0.0]}
   for hist in hists.values():
-    for mu_type in ("c", "f"):
-      count_and_er = get_integral_and_er_for_muon_type(hist, mu_type)
-      hist.param[mu_type] = count_and_er
-      total_ints[mu_type][0] += count_and_er[0]
-      total_ints[mu_type][1] += (count_and_er[1]/count_and_er[0])**2
+    for mu_type in total_ints:
+      # count_and_er = get_integral_of_sub_fit(hist, mu_type)
+      scale = [val/bin_width for val in hist.param["N_"+mu_type]]
+      tau   = hist.param["tau_"+mu_type]
+      integral, int_er  = get_integral_and_error_on_exp(scale, tau)
+      hist.param[mu_type] = integral, int_er
+      total_ints[mu_type][0] += integral
+      if integral == 0: 
+        print "WARNING: integral = 0"
+        total_ints[mu_type][1] += 0.0 # this shouldn't really happen
+      else:
+        total_ints[mu_type][1] += (int_er/integral)**2
+        
   # Correctly calculate the errors
-  for mu_type in ("c", "f"):
-    total_ints[mu_type][1] = total_ints[mu_type][0] * total_ints[mu_type][1]**0.5
+  for key in total_ints:
+    total_ints[key][1] = total_ints[key][0] * total_ints[key][1]**0.5
   
-  total_ints["R"][0] = total_ints["c"][0]/total_ints["f"][0]
-  dc_c = (total_ints["c"][1]/total_ints["c"][0])**2
-  df_f = (total_ints["f"][1]/total_ints["f"][0])**2
-  total_ints["R"][1] = total_ints["R"][0]*(dc_c + df_f)**0.5
+  total_ints["R"] = get_ratio(total_ints[target_mat], total_ints["f"])
   return total_ints
 
-def get_integral_and_er_for_muon_type(hist, mu_type):
-  mu_type = mu_type.lower()
+def get_ratio(numerator_and_er, denom_and_er):
+  ratio = numerator_and_er[0]/denom_and_er[0]
+  d_numerate = (numerator_and_er[1]/numerator_and_er[0])**2
+  d_denominator = (denom_and_er[1]/denom_and_er[0])**2
   
-  scale = hist.param["N_f"]   if (mu_type == "f") else hist.param["N_c"]
-  tau   = hist.param["tau_f"] if (mu_type == "f") else hist.param["tau_c"]
-  # Root takes submatrix selections in an x1,x2,y1,y2 form (0,0)=N_b
-  sub_matrix =  (3,4,3,4) if (mu_type == "f") else (1,2,1,2)
-  covar_sub  = hist.covar_matrix.GetSub(*sub_matrix)
-  
-  func = get_tmp_func(scale, tau)
-  
-  integral    = func.Integral(0, 20000)
-  integral_er = func.IntegralError(0, 20000, func.GetParameters(), covar_sub.GetMatrixArray())
-  return integral, integral_er
+  ratio_er = ratio * (d_numerate + d_denominator) **0.5
+  return ratio, ratio_er
 
-def get_tmp_func(scale, tau):
-  res = TF1("tmp_func", "[0]*exp(-x/[1])", 0, 20000)
+
+def get_integral_and_error_on_exp(scale_and_er, tau_and_er, start=50, stop=20000):
+  func = get_tmp_func(scale_and_er, tau_and_er, start, stop)
+  integral = func.Integral(start, stop)
+  d_scale = (scale_and_er[1]/scale_and_er[0]) ** 2
+  d_tau   = (tau_and_er[1]/tau_and_er[0]) ** 2
+  return integral, integral*(d_scale + d_tau)**0.5
+
+def get_tmp_func(scale, tau, start, stop):
+  res = TF1("tmp_func", "[0]*exp(-x/[1])", start, stop)
   set_param_and_error(res, 0, *scale)
   set_param_and_error(res, 1, *tau)
   return res
@@ -255,7 +258,7 @@ def set_param_and_error(func, param, val, er):
 # = Pretty Printer =
 # ==================
 def print_results(hists, file_id, integrals):
-  data_list = list(map(lambda x:clean_par_name(x[0]), fitting_parameters)) + ["c",] + ["f",]
+  data_list = list(map(lambda x:clean_par_name(x[0]), fitting_parameters)) + [target_mat,] + ["f",]
   # Get the format string and add the channel at the start
   table_header, data_fmt = get_table_header_and_fmt_string(data_list, u"ch ", u"{ch} ")
   print table_header
@@ -276,12 +279,30 @@ def get_table_header_and_fmt_string(data_list, header=u"", data_fmt=u"", div=u"|
 # =================
 def main():
   degraders = (0.0, 0.5, 1.0, 5.0)
+  # degraders = (2.5, 5, 7.5, 15)
+  # data_files = {2.5:(468,), 5:(466,), 7.5:(464,), 15:(508,)}
+    # 
+                
+  # data_files = {0.5: (451,),} # FAST  
+  # channels = ("D1",)  # FAST
+  channels = ("D1", "D2", "D3", "D4", "D5")
+  data_files = file_stats = {}
   
-  data_files = {0.0: (448, ),
-                  0.5: (451, 452),
-                  1.0: (455, ),
-                  5.0: (458, 459)}
-  # data_files = {0.5: (451,),} # FAST
+  if target_mat == "Cu":
+    file_stats = {448:{ 'time': 9221, 'current':0.0153375  , 'acceptance':0.087},
+                  451:{ 'time': 1001, 'current':0.0154625  , 'acceptance':0.077},
+                  452:{ 'time': 4944, 'current':0.013132143, 'acceptance':0.077},
+                  455:{ 'time': 6307, 'current':0.013321429, 'acceptance':0.069},
+                  458:{ 'time': 5144, 'current':0.013625   , 'acceptance':0.045},
+                  459:{ 'time': 2452, 'current':0.012383929, 'acceptance':0.045},}
+    data_files = {0.0: (448, ), 0.5: (451, 452), 1.0: (455, ), 5.0: (458, 459)}
+  else:
+    file_stats = {464: {'time': 5884,  'current':0.4096, 'acceptance':0.087},
+                  466: {'time': 7220,  'current':0.3477, 'acceptance':0.087},
+                  468: {'time': 13460, 'current':0.3220, 'acceptance':0.087},
+                  508: {'time': 4129,  'current':0.3   , 'acceptance':0.087}}
+    data_files = {2.5:(468,), 5:(466,), 7.5:(464,), 15:(508,)}
+                
   g4bl_file = "~/code/MuSIC/simulation/MuSIC_5_detector_sim/"+\
               "MuSIC5/g4blout/from_hep_1Bn/"+\
               "g4bl_out_36_rotation_30435841_particles.root"
@@ -291,16 +312,15 @@ def main():
   gStyle.SetOptStat(10)
   gStyle.SetOptFit(111)
   
-  # channels = ("D1",)  # FAST
-  channels = ("D1", "D2", "D3", "D4", "D5")
   integrals = {}
   for degZ, file_roots in data_files.items():
     for file_id in file_roots:
       print file_id
-      img_name = u"images/fit_data_file_{}_bin_width_{}_tau_Cu_tight".format(file_id, bin_width)
+      img_name = u"images/fit_data_file_{}_bin_width_{}_{}_loose".format(file_id, bin_width, target_mat) if not FAST else ""
       file_ints  = process_data_file(file_id, channels, img_name)
       integrals[file_id] = file_ints
-      # return # FAST
+    if FAST: break
+      
       
   print "="*80
   print "Settings:"
@@ -308,7 +328,8 @@ def main():
   print "\tFunc format", func_fmt
   print "\tInitial fit settings:"
   print getsource(fitting_parameters[0][1]) # will print the whole thing
-  
+  print "\tFile info", 
+  for i,j in file_stats.items(): print i, j
   print "="*80
   print "G4BL predicts: "
   for key in  ("mu-", "mu+", "ratio"):
@@ -316,11 +337,26 @@ def main():
     print pretty_val_and_er(key, "8.2", "8.2").format(**sim_n_mu)
   
   print "Data says: "
-  data_header, data_fmt = get_table_header_and_fmt_string( ("c", "f", "R"),\
-                          header=u"Run ", data_fmt=u"{file_id} ", val_places="8.2", er_places="8.2")
+  data_header, data_fmt = get_table_header_and_fmt_string( (target_mat, "f", "R"),\
+  # data_header, data_fmt = get_table_header_and_fmt_string( ("Cu", "f", "R"),\
+                          header=u"Run ", data_fmt=u"{file_id} ", val_places="11.3", er_places="11.3")
+  print data_header
+  for file_id, int_and_er in integrals.items():
+    print data_fmt.format(file_id=file_id, **int_and_er)
+  
+  print "\nAs normalised rates: (integral)/(time (s)*current (ns)*acceptance)"
+  print "Mg data for 15mm is missing proton current (assumed as 0.3nA), acceptances need to be calculated"
+  
+  data_header, data_fmt = get_table_header_and_fmt_string( ("rate_target", "rate_f"), header=u"Run ", data_fmt=u"{file_id} ")
   print data_header
   for file_id, integral in integrals.items():
-    print data_fmt.format(file_id=file_id, **integral)
+    # print integral
+    rate_target, rate_f = [0,0], [0,0]
+    rate_target[0] = float(integral[target_mat][0])/(file_stats[file_id]['time']*file_stats[file_id]['current']*file_stats[file_id]['acceptance'])
+    rate_target[1] = float(integral[target_mat][1])/(file_stats[file_id]['time']*file_stats[file_id]['current']*file_stats[file_id]['acceptance'])
+    rate_f[0] = float(integral["f"][0])/(file_stats[file_id]['time']*file_stats[file_id]['current']*file_stats[file_id]['acceptance'])
+    rate_f[1] = float(integral["f"][1])/(file_stats[file_id]['time']*file_stats[file_id]['current']*file_stats[file_id]['acceptance'])
+    print data_fmt.format( file_id=file_id, rate_target=rate_target, rate_f=rate_f)
 
 if __name__=="__main__":
   main()
