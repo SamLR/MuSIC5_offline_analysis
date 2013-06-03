3d73d69ee93feaaf82c3b99d6a80f3c357b29470
Sam Cook <sam@samlr.com>
2013-06-03 15:33:45 +0100

    Updated version of dead time to account for changed root utilities, moved valueWithEr to site-packages, killed sclr func (no longer needed)

--- MuSIC5_offline_analysis/scripts/dead_time_estimation.py
+++ MuSIC5_offline_analysis/scripts/dead_time_estimation.py
@@ -3,56 +3,68 @@ Calculate the dead time for each run by dividing the number of triggers
 counted by the scaler by the number of possible triggers.
 """
 
-from sclr_branch_func import * 
+from root_utilities import get_tree_from_file
+from ValueWithError import ValueWithError
 
-def get_dead_time_from_file(file_data, file_root):
-    name = file_prefix + file_root + file_suffix
-    print "Opening file: %5s, tree: %s" % (file_root, file_data['tree_name'])
-    # need tfile to keep it in scope
-    tfile, tree, branch = get_tfile_tree_and_branch(name, **file_data)
-    # we need the last entry
-    n_entries = tree.GetEntries()
-    tree.GetEntry(n_entries - 1)
-    
-    # U&&!D including & excluding veto and error
-    exc_veto = float(branch['scaler'][1]), (float(branch['scaler'][1]))**0.5
-    inc_veto = float(branch['scaler'][2]), (float(branch['scaler'][2]))**0.5
-    
-    
-    dead_time    = exc_veto[0]/inc_veto[0]
-    sq_frac = lambda x: (x[1]/x[0])**2 # (dA/A) ^ 2
-    dead_time_er = dead_time * ((sq_frac(inc_veto) + sq_frac(exc_veto))**0.5)
-    
-    
-    return {'u_not_d_not_veto':exc_veto, 'u_not_d':inc_veto, 
-            'dead_time':(dead_time,dead_time_er)}
+file_name_fmt = "/Users/scook/code/MuSIC/offline_analysis_music5/MuSIC_data_0.5Cu/run{run_id}.root"
+run_ids = (448, 451, 452, 455, 458, 459)
+
+def assign_leaf(tree, branch_name, leaf_name):
+  """
+  This is mostly a hack to make the leaves behave like
+  native attributes of the tree.
+  """
+  tree.branch = tree.GetBranch(branch_name)
+  # Add the leaves to their dictionaries
+  tree.scaler_leaf = tree.branch.GetLeaf(leaf_name)
+  # Add accessor functions, TDC needs an array index. 
+  tree.scaler = lambda x: float(tree.scaler_leaf.GetValue(x))
+
+def get_dead_time_from_file(run_id):
+  if run_id == 448:
+    tree_name, branch_name, leaf_name = "ts", "Scaler", "scaler"
+  else:
+    run_id = "00{}".format(run_id)
+    tree_name, branch_name, leaf_name = "Scaler", "SCLR", "SCLR"
+  print_fmt ="Opening file with id:{}, tree:{}, branch:{}, leaf:{}"
+  print print_fmt.format(run_id, tree_name, branch_name, leaf_name)
+  
+  file_name = file_name_fmt.format(run_id=run_id)
+  tree = get_tree_from_file(tree_name, file_name)
+  assign_leaf(tree, branch_name, leaf_name)
+  
+  # we need the last entry
+  n_entries = tree.GetEntries()
+  tree.GetEntry(n_entries - 1)
+  
+  # U&&!D including & excluding veto and error
+  res = {'u_not_d_not_veto':ValueWithError(tree.scaler(1)), 'u_not_d':ValueWithError(tree.scaler(2))}
+  for i in res.values():
+    i.print_fmt=u"{: >9.0f} +/- {: <4.0f}"
+  res['dead_time'] = res['u_not_d_not_veto']/res['u_not_d']
+  res['dead_time'].print_fmt = u"{: >5.2f} +/- {: <5.3f}"
+  return res
 
 
-def print_pretty_results(dead_time_dicts):
-    import locale
-    locale.setlocale(locale.LC_ALL, 'en_US')
-    locale.format("%d", 1255000, grouping=True)
-    print "%4s | %13s | %16s | %18s"%("file", "dead time (%)", "(U && !D)", "(U && !D && !Veto)")
-    ord_keys = list(dead_time_dicts.keys())
-    ord_keys.sort()
-    for i in ord_keys:
-        dt            = 100*dead_time_dicts[i]['dead_time'][0]
-        dt_er         = 100*dead_time_dicts[i]['dead_time'][1]
-        u_nd          = int(dead_time_dicts[i]['u_not_d'][0])
-        u_nd_er       = int(dead_time_dicts[i]['u_not_d'][1])
-        u_nd_nveto    = int(dead_time_dicts[i]['u_not_d_not_veto'][0])
-        u_nd_nveto_er = int(dead_time_dicts[i]['u_not_d_not_veto'][1])
-        # print "%4i | %7.1f %% | %9i | %18i"%(int(i), dt, u_nd, u_nd_nveto)
-        print u"%4i | % 6.2f \xb1 %.2f | %9i \xb1 %i | %7i \xb1 %i"\
-                %(int(i), dt, dt_er, u_nd, u_nd_er, u_nd_nveto, u_nd_nveto_er)
+def get_pretty_results(dead_time_dicts):
+  res = u"{:^3s} | {:^15s} | {:^18s} | {:^17s}\n".format("Run", "dead time (%)", "(U && !D)", "(U && !D && !Veto)")
+  ord_keys = [int(run_id) for run_id in dead_time_dicts.keys()]
+  ord_keys.sort()
+  entry_fmt=u"{run_id} | {dead_time_per} | {u_not_d} | {u_not_d_not_veto}\n"
+  for i in ord_keys:
+    dead_time_per   = 100*dead_time_dicts[i]['dead_time'] # make it a percent
+    dead_time_per.print_fmt = dead_time_dicts[i]['dead_time'].print_fmt
+    res += entry_fmt.format(run_id=run_id, dead_time_per=dead_time_per, **dead_time_dicts[i])
+  return res
 
+def get_dead_times_for_run_ids(run_ids):
+  return {r:get_dead_time_from_file(r) for r in run_ids}
 
 def main():
-    res = {}
-    for file_id, file_data in files_info.items():
-        res[int(file_id)] = get_dead_time_from_file(file_data, file_id)
-    print '\n'
-    print_pretty_results(res)
+  res = get_dead_times_for_run_ids(run_ids)
+  print get_pretty_results(res)
+  print "*"*80
+  print res
 
 if __name__=="__main__":
-    main()
+  main()
